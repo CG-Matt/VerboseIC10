347d9b36ba598e620e5f39c604ac9cef233eefa1
CG-Matt <thecubeplayzgames@gmail.com>
2023-08-03 02:02:42 +0100

    Fixed typos and added a new string helper function

--- src/utils.h
+++ src/utils.h
@@ -10,15 +10,18 @@ bool is_number(const std::string& str);
 bool is_boolean(const std::string& variable);
 bool is_reference(const std::string& variable);
 
+// String helpers
+bool starts_with(const std::string& data, const std::string& segment);
+
 // Vector helpers
 std::string join_string(const std::vector<std::string>& data, std::string delim);
-std::vector<std::string> split_string(std::string str, char delim);
+std::vector<std::string> split_string(const std::string& str, char delim);
 std::string shift(std::vector<std::string>& data);
 bool includes(const std::vector<std::string>& data, std::string search_string);
 
 // Custom data parsing
 std::string parse_boolean(const std::string& variable);
-std::string parse_value(const std::string& variavle, const ParserGlobals& globals);
+std::string parse_value(const std::string& variable, const ParserGlobals& globals);
 std::vector<std::string> parse_array(const std::vector<std::string>& data);
 
 // Comparing functions
